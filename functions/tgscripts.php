c1b37300d0fa718ac0251682a2e667cb6c300a96
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2013-10-04 10:50:09 +0300

    Remove annoying permanent ad visible on all pages in admin area, which on top of that had buggy JavaScript

--- functions/tgscripts.php
+++ functions/tgscripts.php
@@ -1,7 +1,7 @@
 <?php 
 /*cargando scripts ene el header*/
 function tg_js_head() {
-    echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" ></script>  ';
+    echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" ></script>  ';
 }
 add_action('admin_head', 'tg_js_head');
 add_action('wp_head', 'tg_js_head',9999);
