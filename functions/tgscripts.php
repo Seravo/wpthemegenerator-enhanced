2c05b6e558cac7081b252934a0ea42422ef6c7c8
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2013-10-09 11:57:42 +0300

    Removed more crap code that breaks JQuery-UI in onther plugins

--- functions/tgscripts.php
+++ functions/tgscripts.php
@@ -1,16 +1,2 @@
 <?php 
-/*cargando scripts ene el header*/
-function tg_js_head() {
-    echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" ></script>  ';
-}
-add_action('admin_head', 'tg_js_head');
-add_action('wp_head', 'tg_js_head',9999);
-
-/*Coloque aqui los scripts en el footer*/
-if ( ! function_exists('tgft_print_script') ){	
-	function tgft_print_script(){
-		global $footer_script;
-		echo $footer_script;
-	}
-}
 ?>
